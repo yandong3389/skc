941b231fe1e048aab71daef21942309ee64a4740
wangruichen <wangruichen_x00108@baosight.com>
2020-06-16 21:57:09 +0800

    ''

--- src/main/java/com/app/skc/controller/WalletController.java
+++ src/main/java/com/app/skc/controller/WalletController.java
@@ -10,6 +10,9 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
 
 import java.io.IOException;
 
@@ -21,6 +24,8 @@ import java.io.IOException;
 public class WalletController {
 	@Autowired
 	private  WalletService walletService;
+	@Autowired
+	private Web3j web3j;
 
 	/**
 	 * 获取用户钱包余额信息
@@ -29,8 +34,11 @@ public class WalletController {
 	 * @return
 	 */
 	@GetMapping("/balance")
-	public ResponseResult query(String userId, String walletType){
-		return null;
+	public ResponseResult query(String userId, String walletType) throws IOException, CipherException {
+
+		Credentials credentials = WalletUtils.loadCredentials("", "/Users/Dylan/Desktop/wallet/UTC--2020-06-13T06-41-47.382000000Z--5bc413403be2d5c0503e89b569b42c0b8f690273.json");
+		System.out.println(JSONObject.toJSONString(credentials));
+         return null;
 	}
 
 	/**
