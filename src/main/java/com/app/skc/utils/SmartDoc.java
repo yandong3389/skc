94398cbd1a0ef3ee490d495190e586991eb502b3
wangruichen <wangruichen_x00108@baosight.com>
2020-06-17 18:14:33 +0800

    生成 api 文档

---
+++ src/main/java/com/app/skc/utils/SmartDoc.java
@@ -0,0 +1,24 @@
+package com.app.skc.utils;
+
+import com.power.doc.builder.ApiDocBuilder;
+import com.power.doc.model.ApiConfig;
+
+public class SmartDoc {
+
+    public static void main(String[] args) {
+        create();
+    }
+    public static void create() {
+        ApiConfig config = new ApiConfig();
+        //服务地址
+        config.setServerUrl("http://localhost:8010");
+        //生成到一个文档
+        config.setAllInOne(true);
+        //采用严格模式
+        config.isStrict();
+        //文档输出路径
+        config.setOutPath("/Users/Dylan/项目/api");
+        ApiDocBuilder.buildApiDoc(config);
+        //将生成的文档输出到/Users/dujf/Downloads/md目录下，严格模式下api-doc会检测Controller的接口注释
+    }
+}
