7207bdef30a64a21c54b29a6f8ece4f873f1cd0e
wangruichen <wangruichen_x00108@baosight.com>
2020-06-18 00:26:44 +0800

    更改接口地址 合约 mock数据

--- src/main/java/com/app/skc/controller/ContractController.java
+++ src/main/java/com/app/skc/controller/ContractController.java
@@ -1,8 +1,20 @@
 package com.app.skc.controller;
 
+import com.app.skc.enums.TransTypeEum;
+import com.app.skc.model.Contract;
+import com.app.skc.model.Transaction;
+import com.app.skc.service.ContractService;
+import com.app.skc.utils.viewbean.Page;
 import com.app.skc.utils.viewbean.ResponseResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * 合约制度
@@ -10,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/contract")
 @RestController
 public class ContractController {
+    @Autowired
+    private ContractService contractService;
+    private static final Logger logger = LoggerFactory.getLogger(ContractController.class);
+    private static final String LOG_PREFIX = "[合约服务] - ";
+
 
     /**
      * 购买合约
@@ -17,18 +34,21 @@ public class ContractController {
      * @param userId 用户id
      * @return
      */
-    @RequestMapping("/buy")
+    @PostMapping("/buy")
     public ResponseResult buy(String id,String userId){
-        return new ResponseResult();
+        return  ResponseResult.success();
     }
 
     /**
      * 获取合约信息
-     * @return
+     * @return list
      */
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public ResponseResult getContracts(){
-        return new ResponseResult();
+        logger.info("{}开始查询合约列表",LOG_PREFIX);
+        List<Contract> list = contractService.list();
+        logger.info("{}查询成功,查询到[{}]条记录",LOG_PREFIX,list.size());
+        return ResponseResult.success("查询成功",list);
     }
 
     /**
@@ -36,9 +56,18 @@ public class ContractController {
      * @param userId 用户 ID
      * @return
      */
-    @RequestMapping("history")
-    public ResponseResult getHistory(String userId){
-        return new ResponseResult();
+    @GetMapping("history")
+    public ResponseResult getHistory(Page page,@RequestParam String userId){
+        Transaction transaction = new Transaction();
+        transaction.setFromUserId(userId);
+        transaction.setPrice(new BigDecimal(1000));
+        transaction.setTransType(TransTypeEum.CONTRACT.getCode());
+        transaction.setCreateTime(new Date());
+        transaction.setModifyTime(new Date());
+        List list = new ArrayList();
+        list.add(transaction);
+        list.add(transaction);
+        return ResponseResult.success().setData(new PageInfo<>(list));
     }
 
 }
---
+++ src/main/java/com/app/skc/service/ContractService.java
@@ -0,0 +1,14 @@
+package com.app.skc.service;
+
+import com.app.skc.model.Contract;
+import com.baomidou.mybatisplus.service.IService;
+
+import java.util.List;
+
+public interface ContractService extends IService <Contract> {
+    /**
+     * 查询合约列表
+     * @return list
+     */
+    List<Contract>list();
+}
