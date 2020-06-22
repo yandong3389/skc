e09acad931b486f2f6958dff56ed6fcbb030b342
wangruichen <wangruichen_x00108@baosight.com>
2020-06-22 11:35:20 +0800

    提交用户收益表

---
+++ src/main/java/com/app/skc/model/Income.java
@@ -0,0 +1,54 @@
+package com.app.skc.model;
+
+import com.baomidou.mybatisplus.activerecord.Model;
+import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 收益表
+ */
+@Data
+@TableName("skc_income")
+public class Income extends Model <Income> {
+    @Id
+    private String id;
+    /**
+     * 用户 id
+     */
+    private String userId;
+    /**
+     * 合约 id
+     */
+    private String contractId;
+    /**
+     * 静态收益
+     */
+    private BigDecimal staticIn;
+    /**
+     * 分享收益
+     */
+    private BigDecimal shareIn;
+    /**
+     * 管理收益
+     */
+    private BigDecimal manageIn;
+
+    /**
+     * 总收益
+     */
+    private BigDecimal total;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    @Override
+    protected Serializable pkVal() {
+        return null;
+    }
+}
