e87dd7fad21680ef7531360479e6f11b62a4dd29
wangruichen <wangruichen_x00108@baosight.com>
2020-06-22 14:12:52 +0800

    购买合约和团队业界开发

--- src/main/java/com/app/skc/controller/ContractController.java
+++ src/main/java/com/app/skc/controller/ContractController.java
@@ -3,6 +3,7 @@ package com.app.skc.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.app.skc.enums.TransTypeEum;
+import com.app.skc.exception.BusinessException;
 import com.app.skc.model.Contract;
 import com.app.skc.model.Transaction;
 import com.app.skc.service.ContractService;
@@ -40,6 +41,12 @@ public class ContractController {
     @PostMapping("/buy")
     public ResponseResult buy(@RequestParam String userId,@RequestParam String code){
         logger.info("{}开始购买合约,请求参数[userId={},code={}]",LOG_PREFIX,userId,code);
+        try {
+            contractService.buy(userId, code);
+        } catch (BusinessException be) {
+            logger.error("{}购买失败:[{}]", LOG_PREFIX, be);
+            return ResponseResult.fail("ERR500", be.getMessage());
+        }
         logger.info("{}购买完成",LOG_PREFIX);
         return  ResponseResult.success();
     }
@@ -79,5 +86,29 @@ public class ContractController {
         logger.info("{}查询成功,查询到[{}]条记录", LOG_PREFIX, list.size());
         return ResponseResult.success().setData(new PageInfo<>(list));
     }
+
+    /**
+     * 查询团队业绩
+     *
+     * @param userIds 自己和伞下所有用户Id的jsonArray
+     * @return
+     */
+    @PostMapping("/performance")
+    public ResponseResult teamPerformance(@RequestBody JSONObject userIds) {
+        String ownerUserId = userIds.getString("ownerUserId");
+        logger.info("{}开始查询用户:[{}]团队[{}]的业绩", LOG_PREFIX, ownerUserId, JSON.toJSONString(userIds));
+        List list = userIds.getJSONArray("userIds");
+        try {
+            return ResponseResult.success("查询成功", contractService.teamPerformance(list));
+        } catch (BusinessException be) {
+            logger.error(be.getMessage());
+            return ResponseResult.fail();
+        }
+    }
+
+
+
+
+
 
 }
