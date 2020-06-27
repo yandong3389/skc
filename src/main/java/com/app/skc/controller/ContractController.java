7110922a9fc89a02457a32b86f03df6b4936ee79
wangruichen <wangruichen_x00108@baosight.com>
2020-06-27 18:58:05 +0800

    获取用户合约接口

--- src/main/java/com/app/skc/controller/ContractController.java
+++ src/main/java/com/app/skc/controller/ContractController.java
@@ -2,10 +2,12 @@ package com.app.skc.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.app.skc.enums.TransStatusEnum;
 import com.app.skc.enums.TransTypeEum;
 import com.app.skc.exception.BusinessException;
 import com.app.skc.mapper.TransactionMapper;
 import com.app.skc.model.Contract;
+import com.app.skc.model.Transaction;
 import com.app.skc.service.ContractService;
 import com.app.skc.utils.SkcConstants;
 import com.app.skc.utils.viewbean.Page;
@@ -16,6 +18,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -76,12 +79,32 @@ public class ContractController {
     @GetMapping("history")
     public ResponseResult getHistory(Page page, @RequestParam String userId) {
         logger.info("{}开始查询合约购买历史,page=[{}],userId=[{}]", LOG_PREFIX, JSON.toJSONString(page), userId);
-        Map map = new HashMap();
-        map.put(SkcConstants.FROM_USER_ID, userId);
-        map.put(SkcConstants.TRANS_TYPE, TransTypeEum.CONTRACT.getCode());
+        Map map = getQueryMap(userId, 5);
         List list = transactionMapper.selectByMap(map);
         logger.info("{}查询成功,查询到[{}]条记录", LOG_PREFIX, list.size());
         return ResponseResult.success().setData(new PageInfo<>(list));
+    }
+
+    private Map getQueryMap(@RequestParam String userId, int i) {
+        Map map = new HashMap(i);
+        map.put(SkcConstants.FROM_USER_ID, userId);
+        map.put(SkcConstants.TRANS_TYPE, TransTypeEum.CONTRACT.getCode());
+        return map;
+    }
+
+    @GetMapping("userContract")
+    public ResponseResult getUserContract(String userId) {
+        logger.info("{}开始查询用户合约,userId=[{}]", LOG_PREFIX, userId);
+        Map map = getQueryMap(userId, 3);
+        map.put(SkcConstants.TRANS_STATUS, TransStatusEnum.EFFECT.getCode());
+        List <Transaction> list = transactionMapper.selectByMap(map);
+        logger.info("{}查询成功,查询到[{}]条记录", LOG_PREFIX, list.size());
+        BigDecimal contractPrice = BigDecimal.ZERO;
+        if (list.size() > 0) {
+            contractPrice = list.get(0).getPrice();
+        }
+        return ResponseResult.success().setData(contractPrice);
+
     }
 
     /**
