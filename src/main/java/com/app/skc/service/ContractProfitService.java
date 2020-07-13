d7d84ecb09cf281a9e3bc5065c707225331fe759
AmazingJerry <ack_qadac@163.com>
2020-07-13 10:43:36 +0800

    合约释放时间前置到23.50，账期指定为下一日期

--- src/main/java/com/app/skc/service/ContractProfitService.java
+++ src/main/java/com/app/skc/service/ContractProfitService.java
@@ -11,7 +11,10 @@ public interface ContractProfitService {
      * 分享合约用户树收益处理
      *
      * @param userShare
+     * @param rate
+     * @param dateAcct
+     * @throws BusinessException
      */
-    void userTreeTrans(UserShareVO userShare, BigDecimal rate) throws BusinessException;
+    void userTreeTrans(UserShareVO userShare, BigDecimal rate, String dateAcct) throws BusinessException;
 
 }
